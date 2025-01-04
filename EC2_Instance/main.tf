e4887507a928f8dd3d9da4c95c7add19e05cdf0b
Murari <70143030+NMurari7@users.noreply.github.com>
2025-01-04 23:50:42 +0100

    Update main.tf

--- EC2_Instance/main.tf
+++ EC2_Instance/main.tf
@@ -5,5 +5,8 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-053b12d3152c0cc71"
     instance_type = "t2.micro"
-    key_name = "ansible"
+    key_name = "<secret_key_name>"
+    tags = {
+        Name = "Terraform_EC2_Instance"  
+  }
 }
